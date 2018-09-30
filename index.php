48c3f8befcf2bfd018a400ef2775ddc5eb7b8ec2
wukunmeng <wukunmeng@xinpianchang.com>
2018-09-30 17:38:15 +0800

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP-Info</title>
+</head>
+<body>
+<?php
+phpinfo();
+echo "<br/>";
+echo '<a href="test.php">呵呵</a>';
+?>
+
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP-Info</title>
+</head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wukm
+ * Date: 18-9-30
+ * Time: 下午2:13
+ */
+
+echo date("Y-m-d");
+
+?>
+
+</body>
+</html>
